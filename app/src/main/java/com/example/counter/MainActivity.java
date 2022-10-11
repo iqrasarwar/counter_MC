acfd240f53244245cac652fbfc03e51103a4bf57
iqrasarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-10-11 23:35:54 +0500

    final commit

--- app/src/main/java/com/example/counter/MainActivity.java
+++ app/src/main/java/com/example/counter/MainActivity.java
@@ -2,14 +2,19 @@ package com.example.counter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView t;
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        t = findViewById(R.string.countNum);
     }
 }
 //amatM012
